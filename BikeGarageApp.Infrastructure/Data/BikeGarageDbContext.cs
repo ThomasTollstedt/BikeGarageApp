e083ce7ba470e1ba8278a421fc3e60b0b68b8d72
Thomas Tollstedt <thomastollstedt@gmail.com>
2026-05-06 11:23:46 +0200

    Init migration, added OnModelCreating for Milage prop. Updated Db

--- BikeGarageApp.Infrastructure/Data/BikeGarageDbContext.cs
+++ BikeGarageApp.Infrastructure/Data/BikeGarageDbContext.cs
@@ -7,10 +7,18 @@ namespace BikeGarageApp.Infrastructure.Data
     {
         public BikeGarageDbContext(DbContextOptions<BikeGarageDbContext> options)
             : base(options)
-        {
-            
-        }
-
+        { }
+        
         public DbSet<Bike> Bikes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); // Alltid bra att ha kvar
+
+            // Här bekräftar vi för EF Core exakt vilken precision Milage ska ha
+            modelBuilder.Entity<Bike>()
+                .Property(b => b.Milage)
+                .HasPrecision(18, 2);
+        }
     }
 }
---
+++ BikeGarageApp.Infrastructure/Migrations/20260506092038_init.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BikeGarageApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bikes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SerialNumber = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ModelName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Model = table.Column<int>(type: "int", nullable: false),
+                    Tier = table.Column<int>(type: "int", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    Size = table.Column<int>(type: "int", nullable: false),
+                    Milage = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    URLPicture = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bikes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bikes");
+        }
+    }
+}
